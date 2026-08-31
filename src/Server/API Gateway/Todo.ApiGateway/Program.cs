3a11ca3a629fe9b9719d458298d9c1ae3ea004f8
omid <omiddadvar94@gmail.com>
2026-08-31 18:44:40 +0330

    feat : Adding-Logging-And-SEQ-Configs-To-ApiGateway-Project

--- src/Server/API Gateway/Todo.ApiGateway/Program.cs
+++ src/Server/API Gateway/Todo.ApiGateway/Program.cs
@@ -1,7 +1,17 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+Log.Logger = new LoggerConfiguration()
+     .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+     .MinimumLevel.Override("System", LogEventLevel.Error)
+     .Enrich.FromLogContext()
+     .ReadFrom.Configuration(builder.Configuration)
+     .CreateLogger();
 
 var app = builder.Build();
 
