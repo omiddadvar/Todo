8fb938594be80b85b965e719f1d86e6344acbeee
omid <omiddadvar94@gmail.com>
2026-09-06 17:45:14 +0330

    feat : Implementing-Infrastructure

---
+++ src/Server/Identity/Todo.Identity/Infrastructure/Data/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using Todo.Identity.Domain.Abstractions.Repositories;
+using Todo.Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Todo.Identity.Infrastructure.Data;
+
+
+public class UserRepository : IUserRepository
+{
+    private readonly MainDbContext _context;
+
+    public UserRepository(MainDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> GetByIdAsync(Guid id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<User> GetByEmailAsync(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<User> GetByRefreshTokenAsync(string refreshToken)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.RefreshToken == refreshToken);
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task<IEnumerable<User>> GetActiveUsersAsync()
+    {
+        return await _context.Users.Where(u => u.IsActive).ToListAsync();
+    }
+
+    public async Task<IEnumerable<User>> GetUsersByRoleAsync(string roleName)
+    {
+        // Implement with role checking
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        _context.Users.Update(user);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(User user)
+    {
+        _context.Users.Remove(user);
+        return Task.CompletedTask;
+    }
+
+    public async Task<bool> ExistsByEmailAsync(string email)
+    {
+        return await _context.Users.AnyAsync(u => u.Email == email);
+    }
+}
---
+++ src/Server/Identity/Todo.Identity/Infrastructure/MainDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Todo.Identity.Domain.Entities;
+
+namespace Todo.Identity.Infrastructure
+{
+    public class MainDbContext : IdentityDbContext< User, Role, Guid>
+    {
+        public MainDbContext()
+        {
+
+        }
+    }
+}
