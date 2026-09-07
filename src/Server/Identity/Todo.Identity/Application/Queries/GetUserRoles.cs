62e0f404539c1d4363674d1c30670db748da42c0
omid <omiddadvar94@gmail.com>
2026-09-07 12:01:27 +0330

    feat : Added-GetUserRoles-Command

---
+++ src/Server/Identity/Todo.Identity/Application/Queries/GetUserRoles.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Todo.Identity.Application.Abstractions;
+using Todo.Identity.Application.DTOs;
+using Todo.Identity.Application.Exceptions;
+using Todo.Identity.Domain.Entities;
+
+namespace Todo.Identity.Application.Queries;
+
+public static class GetUserRoles
+{
+    public record Query : IQuery<Result>
+    {
+        public Guid UserId { get; set; }
+    }
+
+    public class Handler(
+        UserManager<User> userManager) : IQueryHandler<Query, Result>
+    {
+        public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var user = await userManager.FindByIdAsync(request.UserId.ToString());
+            if (user == null)
+                throw new UserNotFoundException($"User with ID {request.UserId} not found");
+
+            var roles = await userManager.GetRolesAsync(user);
+
+            return Result.Success(new UserRolesDTO
+            {
+                UserId = user.Id,
+                Email = user.Email,
+                Roles = roles.ToList()
+            });
+        }
+    }
+}
