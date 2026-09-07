09fa89d9657fdd8b2ad67e4c3b7a0258ab6d9986
omid <omiddadvar94@gmail.com>
2026-09-07 12:13:49 +0330

    feat : Added-MassTransit-Service-Config

--- src/Server/Identity/Todo.Identity/Constants/ConfigKeyword.cs
+++ src/Server/Identity/Todo.Identity/Constants/ConfigKeyword.cs
@@ -4,10 +4,17 @@ public static class ConfigKeyword
 {
     public static class Token
     {
-        public const string RefreshTokenExpireInDays = "Token:RefreshTokenExpireInDays";
+        public const string RefreshTokenExpireInDays = "JwtSettings:RefreshTokenExpireInDays";
+        public const string AccessTokenExpireInMinutes = "JwtSettings:AccessTokenExpireInMinutes";
     }
     public static class Appsetting
     {
         public const string BaseUrl = "AppSettings:BaseUrl";
+    }
+    public static class RabbitMQ
+    {
+        public const string Host = "RabbitMQ:Host";
+        public const string Username = "RabbitMQ:Username";
+        public const string Password = "RabbitMQ:Password";
     }
 }
---
+++ src/Server/Identity/Todo.Identity/ServiceConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Todo.Identity.Constants;
+
+namespace Todo.Identity;
+
+public static class ServiceConfigurations
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMasstransitService(configuration);
+        return services;
+    }
+    private static IServiceCollection AddMasstransitService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(configuration[ConfigKeyword.RabbitMQ.Host], "/", h =>
+                {
+                    h.Username(configuration[ConfigKeyword.RabbitMQ.Username]);
+                    h.Password(configuration[ConfigKeyword.RabbitMQ.Password]);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+        return services;
+    }
+}
