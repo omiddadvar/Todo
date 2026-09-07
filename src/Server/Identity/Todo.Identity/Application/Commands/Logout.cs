a73f7384b0bea01ef57fa038bffbdf4946880dc6
omid <omiddadvar94@gmail.com>
2026-09-07 11:36:11 +0330

    feat : Added-Logout-Command-Application-Layer

---
+++ src/Server/Identity/Todo.Identity/Application/Commands/Logout.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Todo.Identity.Application.Abstractions;
+using Todo.Identity.Application.Exceptions;
+using Todo.Identity.Domain.Abstractions;
+using Todo.Identity.Domain.Entities;
+
+namespace Todo.Identity.Application.Commands;
+
+public static class Logout
+{
+    public record Command : ICommand<Result>
+    {
+        public Guid UserId { get; set; }
+    }
+
+    public class Handler(
+        UserManager<User> userManager,
+        IUnitOfWork unitOfWork) : ICommandHandler<Command, Result>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var user = await userManager.FindByIdAsync(request.UserId.ToString());
+            if (user == null)
+                throw new UserNotFoundException($"User with ID {request.UserId} not found");
+
+            user.RevokeRefreshToken();
+            await userManager.UpdateAsync(user);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
---
+++ src/Server/Identity/Todo.Identity/Application/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Identity.Application.Exceptions;
+
+public class UserNotFoundException : ApplicationException
+{
+    private const string CODE = "Identity.Application.UserNotFound";
+
+    public UserNotFoundException(string message) : base(CODE, message)
+    {
+    }
+}
